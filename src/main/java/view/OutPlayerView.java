dc7eebe2898359be96f2d2b0d57ef9d89840d782
khsrla9806 <kimhunsope@naver.com>
2023-06-28 21:49:29 +0900

    feat: Player, OutPlayer View 구현 및 콘솔 테스트 완료
    PlayerView.java 코드 추가
    OutPlayerView.java 코드 추가

--- src/main/java/view/OutPlayerView.java
+++ src/main/java/view/OutPlayerView.java
@@ -1,7 +1,12 @@
 package view;
 
+import model.dto.OutPlayerRespDto;
+import model.player.Player;
 import service.OutPlayerService;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 public class OutPlayerView {
@@ -13,10 +18,51 @@ public class OutPlayerView {
     }
 
     public void create(Map<String, Object> params) {
-
+        try {
+            int playerId = Integer.parseInt(params.get("playerId").toString());
+            String reason = params.get("reason").toString();
+            System.out.println(outPlayerService.createOutPlayer(playerId, reason));
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
     }
 
     public void findOutPlayers() {
+        List<OutPlayerRespDto> outPlayers = outPlayerService.getOutPlayers();
+        String[] tableHead = {"선수Id" ,"이름", "포지션", "퇴출사유", "퇴출일"};
+        printRow(tableHead);
+        for (OutPlayerRespDto player : outPlayers) {
+            String[] tableData = {
+                    player.getPlayerId().toString(),
+                    player.getPlayerName(),
+                    player.getPlayerPosition(),
+                    player.getOutReason(),
+                    getFormatDate(player.getOutDate())
+            };
+            printRow(tableData);
+        }
+    }
 
+    private void printRow(String[] input) {
+        StringBuilder builder = new StringBuilder();
+        for (String str : input) {
+            builder.append(formatData(str));
+        }
+        System.out.println(builder);
+    }
+
+    private String formatData(String data) {
+        if (data == null) {
+            return "";
+        }
+        return String.format("%-8s", data);
+    }
+
+    private String getFormatDate(Timestamp timestamp) {
+        if (timestamp == null) {
+            return "";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY.MM.dd");
+        return timestamp.toLocalDateTime().format(formatter);
     }
 }
