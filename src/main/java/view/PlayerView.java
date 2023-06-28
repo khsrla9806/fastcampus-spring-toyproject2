c084fddee97296fd5ae3461b4b8c2f0d82802a99
khsrla9806 <kimhunsope@naver.com>
2023-06-28 21:49:29 +0900

    feat: Player, OutPlayer View 구현 및 콘솔 테스트 완료
    PlayerView.java 코드 추가
    OutPlayerView.java 코드 추가

--- src/main/java/view/PlayerView.java
+++ src/main/java/view/PlayerView.java
@@ -1,7 +1,12 @@
 package view;
 
+import model.dto.PositionRespDto;
+import model.player.Player;
 import service.PlayerService;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 public class PlayerView {
@@ -12,14 +17,77 @@ public class PlayerView {
     }
 
     public void create(Map<String, Object> params) {
-
+        try {
+            int teamId = Integer.parseInt(params.get("teamId").toString());
+            String name = params.get("name").toString();
+            String position = params.get("position").toString();
+            System.out.println(playerService.createPlayer(teamId, name, position));
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
     }
 
     public void findPlayers(Map<String, Object> params) {
-
+        try {
+            int teamId = Integer.parseInt(params.get("teamId").toString());
+            List<Player> players = playerService.getPlayersByTeamId(teamId);
+            String[] tableHead = {"선수Id" ,"이름", "포지션", "등록일"};
+            printRow(tableHead);
+            for (Player player : players) {
+                String[] tableData = {
+                        player.getId().toString(),
+                        player.getName(),
+                        player.getPosition(),
+                        getFormatDate(player.getCreatedAt())
+                };
+                printRow(tableData);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
     }
 
     public void findPlayersByPosition() {
+        PositionRespDto playersPerPosition = playerService.getPlayersPerPosition();
+        String[] tableHead = new String[playersPerPosition.getTeams().size() + 1];
+        tableHead[0] = "포지션";
+        for (int i = 1; i < tableHead.length; i++) {
+            tableHead[i] = playersPerPosition.getTeams().get(i - 1);
+        }
+        printRow(tableHead);
+        for (String position : PositionRespDto.POSITIONS) {
+            List<String> playerNames = playersPerPosition.getPositionAndPlayerNames().get(position);
+            if (!playerNames.isEmpty()) {
+                String[] tableData = new String[playerNames.size() + 1];
+                tableData[0] = position;
+                for (int i = 1; i <= playerNames.size(); i++) {
+                    tableData[i] = playerNames.get(i - 1);
+                }
+                printRow(tableData);
+            }
+        }
+    }
 
+    private void printRow(String[] input) {
+        StringBuilder builder = new StringBuilder();
+        for (String str : input) {
+            builder.append(formatData(str));
+        }
+        System.out.println(builder);
+    }
+
+    private String formatData(String data) {
+        if (data == null) {
+            return "";
+        }
+        return String.format("%-8s", data);
+    }
+
+    private String getFormatDate(Timestamp timestamp) {
+        if (timestamp == null) {
+            return "";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY.MM.dd");
+        return timestamp.toLocalDateTime().format(formatter);
     }
 }
