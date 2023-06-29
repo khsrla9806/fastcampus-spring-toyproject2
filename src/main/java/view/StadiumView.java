58fc1ee5aec2cfe4d4e7409a4c2a2b17a414e964
jangheepark <jeanparkmk@gmail.com>
2023-06-29 11:28:32 +0900

    feat : Stadium, Team View 구현 StadiumView.java 코드 추가 TeamView.java 코드 추가

--- src/main/java/view/StadiumView.java
+++ src/main/java/view/StadiumView.java
@@ -1,7 +1,11 @@
 package view;
 
+import model.stadium.Stadium;
 import service.StadiumService;
 
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Map;
 
 public class StadiumView {
@@ -12,10 +16,51 @@ public class StadiumView {
     }
 
     public void create(Map<String, Object> params) {
-
+        try {
+            String name = params.get("name").toString();
+            System.out.println(stadiumService.createStadium(name));
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
     }
 
     public void findStadiums() {
+        try {
+            List<Stadium> stadiums = stadiumService.getAllStadiums();
+            String[] tableHead = {"스타디움 ID", "스타디움 이름", "등록일"};
+            printRow(tableHead);
+            for (Stadium stadium : stadiums) {
+                String[] tableData = {
+                        stadium.getId().toString(),
+                        stadium.getName(),
+                        getFormatDate(stadium.getCreatedAt())
+                };
+                printRow(tableData);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("잘못된 요청입니다.");
+        }
 
+    }
+    private void printRow(String[] input) {
+        StringBuilder builder = new StringBuilder();
+        for (String str : input) {
+            builder.append(formatData(str));
+        }
+        System.out.println(builder);
+    }
+
+    private String formatData(String data) {
+        if (data == null) {
+            return "";
+        }
+        return String.format("%-8s", data);
+    }
+    private String getFormatDate(Timestamp timestamp) {
+        if (timestamp == null) {
+            return "";
+        }
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("YYYY.MM.dd");
+        return timestamp.toLocalDateTime().format(formatter);
     }
 }
